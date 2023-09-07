a4781b308082083a18c54c4a3e260ed7e88287ff
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-09-07 15:59:53 +0100

    Mostly finished program

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -35,17 +35,36 @@ remaining_water = resources['water']
 remaining_milk = resources['milk']
 remaining_coffee = resources['coffee']
 
-#Drink variables
+#Drink cost variables
 espresso_price = MENU["espresso"]["cost"]
-latte_price = MENU["latte"]["cost"]
-cappuccino_price = MENU["cappuccino"]["cost"]
+espresso_water_cost = MENU["espresso"]["ingredients"]["water"]
+espresso_coffee_cost = MENU["espresso"]["ingredients"]["coffee"]
+espresso_milk_cost = 0
 
+latte_price = MENU["latte"]["cost"]
+latte_water_cost = MENU["latte"]["ingredients"]["water"]
+latte_coffee_cost = MENU["latte"]["ingredients"]["coffee"]
+latte_milk_cost = MENU["latte"]["ingredients"]["milk"]
+
+cappuccino_price = MENU["cappuccino"]["cost"]
+cappuccino_water_cost = MENU["cappuccino"]["ingredients"]["water"]
+cappuccino_coffee_cost = MENU["cappuccino"]["ingredients"]["coffee"]
+cappuccino_milk_cost = MENU["cappuccino"]["ingredients"]["milk"]
+
+def deduct_resources(water, coffee, milk):
+    global remaining_water
+    global remaining_coffee
+    global remaining_milk
+    remaining_water -= water
+    remaining_coffee -= coffee
+    remaining_milk -= milk
+    
 
 #Print remaining resources when typing report
 def resources_left(water, milk, coffee):
     print(f"Water: {water}ml")
     print(f"Milk: {milk}ml")
-    print(f"Coffee: {coffee}")
+    print(f"Coffee: {coffee}g")
 
 
 def calculate_total_coins():
@@ -73,32 +92,31 @@ def calculate_change(coins, drink,):
         return f"Here is {rounded_change} in change."
 
 
+Coffe_machine_active = True
 
-
-#Input ask for type of drink
-drink = input("What would you like? (espresso/latte/cappuccino) ").lower()
-if drink == "report":
-    resources_left(remaining_water, remaining_milk, remaining_coffee)
-elif drink == "espresso":
-    total_coins = calculate_total_coins()
-    print(calculate_change(total_coins, espresso_price))
-    if total_coins >= espresso_price:
-        print(f"Here is your {drink} ☕️. Enjoy!")
-elif drink == "latte":
-    total_coins = calculate_total_coins()
-    print(calculate_change(total_coins, latte_price))
-    if total_coins >= latte_price:
-        print(f"Here is your {drink} ☕️. Enjoy!")
-elif drink == "cappuccino":
-    total_coins = calculate_total_coins()
-    print(calculate_change(total_coins, cappuccino_price))
-    if total_coins >= cappuccino_price:
-        print(f"Here is your {drink} ☕️. Enjoy!")
-
-
-
-
-
-
-# make a deduction from the resources
-# make a loop until resourcs are not enough
+while Coffe_machine_active:
+    #Input ask for type of drink
+    drink = input(" What would you like? (espresso/latte/cappuccino) ").lower()
+    if drink == "report":
+        resources_left(remaining_water, remaining_milk, remaining_coffee)
+    elif drink == "espresso":
+        total_coins = calculate_total_coins()
+        print(calculate_change(total_coins, espresso_price))
+        deduct_resources(espresso_water_cost, espresso_coffee_cost, espresso_milk_cost)
+        if total_coins >= espresso_price:
+            print(f"Here is your {drink} ☕️. Enjoy!")
+    elif drink == "latte":
+        total_coins = calculate_total_coins()
+        print(calculate_change(total_coins, latte_price))
+        deduct_resources(latte_water_cost, latte_coffee_cost, latte_milk_cost)
+        if total_coins >= latte_price:
+            print(f"Here is your {drink} ☕️. Enjoy!")
+    elif drink == "cappuccino":
+        total_coins = calculate_total_coins()
+        print(calculate_change(total_coins, cappuccino_price))
+        deduct_resources(cappuccino_water_cost, cappuccino_coffee_cost, cappuccino_milk_cost)
+        if total_coins >= cappuccino_price:
+            print(f"Here is your {drink} ☕️. Enjoy!")
+    elif drink == "off":
+        Coffe_machine_active = False
+        print("Shutting down...")
